782fe827a22a236219fa154236fb978ff3395200
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-12-31 21:45:47 +0600

    initial setup

---
+++ android/app/src/main/kotlin/com/example/abacussboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.abacussboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:abacussboard/horizontal_board.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HorMultipleSticks(),
+      // home: MultipleSticks(),
+    );
+  }
+}
